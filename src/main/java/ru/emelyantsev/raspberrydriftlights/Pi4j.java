f69d0d7d4ef7cd53f6773f7d520373a478e82ed0
Evgeny <the0din04ka@gmail.com>
2025-02-12 21:56:43 +0300

    release

--- src/main/java/ru/emelyantsev/raspberrydriftlights/Pi4j.java
+++ src/main/java/ru/emelyantsev/raspberrydriftlights/Pi4j.java
@@ -26,8 +26,8 @@ public class Pi4j {
     private I2C tca9548a;
     private I2C vl53l0x;
    // private I2C vl53l0x2;
-   private int[] tcaChannels = {0, 1};
-   private int[] newAddresses = {0x31, 0x30};
+   private int[] tcaChannels = {1, 2};
+   private int[] newAddresses = {0x31, 0x32};
     private static final ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor();
     private static final Map<Integer, VL53L0X_Device> sensors = new ConcurrentHashMap<>();
 
@@ -60,13 +60,13 @@ public class Pi4j {
 //        Arrays.stream(tcaChannelsreverse).forEach(channel -> {
 //            executor.submit(() -> run(sensors.get(channel), channel));
 //        });
-        executor.submit(() -> run(sensors.get(0), 0));
+        executor.submit(() -> run(sensors.get(0), 1));
         try {
             Thread.sleep(10000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        executor.submit(() -> run(sensors.get(1), 1));
+        executor.submit(() -> run(sensors.get(1), 2));
     }
 
     // Выбор канала на мультиплексоре TCA9548A
